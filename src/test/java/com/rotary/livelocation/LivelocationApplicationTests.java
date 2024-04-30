751550da00fa53ef531c3022b3b5b90e7a4c2892
Hong <chanyikhong89@gmail.com>
2024-05-01 00:14:34 +1000

    init setup

---
+++ src/test/java/com/rotary/livelocation/LivelocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rotary.livelocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivelocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
